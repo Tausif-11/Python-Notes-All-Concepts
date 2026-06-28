26d5a8f6053ea69ef6286a87ab633e0c1aab5bd5
Tausif-11 <tausifmohammad54@gmail.com>
2026-06-28 17:11:55 +0530

    Adding Data Types

---
+++ 02_data_types.py
@@ -0,0 +1,19 @@
+# What are Data types in Python?
+# Data types in Python are classifications that specify the type of data a variable can hold. They define the operations that can be performed on the data and the way the data is stored in memory. Python has several built-in data types, including:
+# 1. Integer (int)
+# 2. Float (float)
+# 3. String (str)
+# 4. Boolean (bool)
+# 5. List (list)
+# 6. Tuple (tuple)
+# 7. Dictionary (dict)
+# 8. Set (set)
+
+# Python has these built-in data types, and you can also create your own custom data types using classes. Each data type has its own characteristics and methods that can be used to manipulate the data. Understanding data types is essential for effective programming in Python, as it helps in writing efficient and error-free code.
+
+# ------------STRINGS AND TYPE CONVERSIONS--------------------------
+
+# Strings are sequences of characters enclosed in single quotes (' ') or double quotes (" "). They are used to represent text data in Python. Strings can be manipulated using various methods and operations, such as concatenation, slicing, and formatting.
+
+# Example of a string:
+my_string = "Hello, World!"
