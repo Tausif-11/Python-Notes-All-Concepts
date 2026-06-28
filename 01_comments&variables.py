cd126d16dab4f18293d553530de98122a13b6553
Tausif-11 <tausifmohammad54@gmail.com>
2026-06-28 07:45:52 +0530

    Adding Chapter 1 Comments & Variables

---
+++ 01_comments&variables.py
@@ -0,0 +1,39 @@
+# This is a comment in Python
+# Comments are used to explain code and are ignored by the Python interpreter
+""" This is a docstring in Python """
+
+# Docstrings are used to describe the purpose of a function, class, or module.
+
+# Variables in Python are used to store data values
+# Variable names can contain letters, numbers, and underscores, but cannot start with a number
+x = 5
+print(x)
+print(type(x))  # This will print the type of the variable x
+
+#types of variables in Python
+# Integer
+# boolean
+# Float
+# String
+
+#Integer type variable
+y = 10
+print(y)
+# print(type(y))  # This will print the type of the variable y
+
+# string type variable
+name = "John" 
+# print(name)
+
+#boolean type variable
+is_active = True
+# print(is_active)
+
+# Float type variable
+z = 10.5
+# print(z)
+
+# print("Hello, World!")  # This is an inline comment
+
+# print("This is a string variable")  # This is another inline comment
+
