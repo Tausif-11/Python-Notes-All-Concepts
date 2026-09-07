115ca59dde464ca3dd0bbce7e7afb66859b58bb0
Tausif-11 <tausifmohammad54@gmail.com>
2026-09-08 00:35:58 +0530

    Adding chapter 3, Inputs & Outputs

---
+++ 03_input&output.py
@@ -0,0 +1,12 @@
+
+"""
+Input and Output
+In Python, input and output are essential for interacting with users. The input() function is used to take input from the user, while the print() function is used to display output on the screen.
+Let's start with the input() function. The input() function allows you to prompt the user for input and returns the input as a string. You can also provide a prompt message to guide the user on what kind of input is expected.
+Here's an example of how to use the input() function: 
+
+prompt = "Please enter your name: "
+name = input(prompt)
+print("Hello, " + name + "!")  # Output: Hello, [user's name]!
+"""
+
