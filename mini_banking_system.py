be3e2b2b85a487adf0ad02306a8d2091b91305f6
Tausif-11 <tausifmohammad54@gmail.com>
2026-09-13 23:01:31 +0530

    Mini Bank project in python

---
+++ mini_banking_system.py
@@ -0,0 +1,23 @@
+
+#              -----------------------
+#                 Mini Banking System
+#              ------------------------
+
+
+print("---------------------------------------------------------")
+print("---------------------------------------------------------")
+print("\n Welcome to the Mini Bank of Mohammad Tausif")
+print("---------------------------------------------------------")
+print("---------------------------------------------------------")
+
+
+# Account Creation
+print("---------------------------------------------------------")
+print(" \n CREATE YOUR ACCOUNT")
+print("---------------------------------------------------------")
+
+print("Please provide the following details to create your account:")
+name = input("Enter your full name: ")
+print(f"Account created for {name}!")
+
+
